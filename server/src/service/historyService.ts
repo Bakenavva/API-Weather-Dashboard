f31e9123f87fb3e68e8f515f3180db0b42ab7664
unknown <ernestoaleman00@gmail.com>
2025-05-03 21:24:30 -0500

    Adjust db name in accordance w/ TODO. Defined some class props

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -6,7 +6,7 @@ const __dirname = path.dirname(__filename);
 
 // TODO: Define a City class with name and id properties
 class City {
-  
+
   constructor(
     public id: number,
     public name: string,
@@ -16,14 +16,29 @@ class City {
 
 // TODO: Complete the HistoryService class
 class HistoryService {
+  filePath: string;
+  cities: City[];
+
+  constructor() {
+    this.filePath = path.join(__dirname, '../../db/searchHistory.json'); 
+    this.cities = [];
+  }
+
   // TODO: Define a read method that reads from the searchHistory.json file
   // private async read() {}
+
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
+  
+  
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
+  
+  
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
+  
+  
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
